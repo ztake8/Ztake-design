f3004c63666623a77ff4c7ab7e9cb5c14dc97087
ztake8 <ztake8@gmail.com>
2025-11-29 13:30:40 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
